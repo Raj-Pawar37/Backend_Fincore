d877d97b00ec56082317f2c7e8a92af9d83e4ed4
Raj-Pawar37 <160260889+Raj-Pawar37@users.noreply.github.com>
2026-07-18 15:55:59 +0530

    Add project files.

---
+++ Backend_Fincore/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend_Fincore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
---
+++ Backend_Fincore/Mapper/MappingData.cs
@@ -0,0 +1,8 @@
+﻿using AutoMapper;
+
+namespace Backend_Fincore.Mapper
+{
+    public class MappingData : Profile
+    {
+    }
+}
