b632ec7abafbf32f065836c94c093e6a0be6b071
Raj-Pawar37 <160260889+Raj-Pawar37@users.noreply.github.com>
2026-07-26 19:33:25 +0530

    Auth Service Part is Done

---
+++ Backend_Fincore.Application/DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Fincore.Application.DTOs.Auth
+{
+    public class LoginResponseDto
+    {
+        public int UserId { get; set; }
+        public bool Is2FAEnabled { get; set; }
+        public bool Requires2FA { get; set; }
+        public string Message { get; set; } = null!;
+    }
+}
