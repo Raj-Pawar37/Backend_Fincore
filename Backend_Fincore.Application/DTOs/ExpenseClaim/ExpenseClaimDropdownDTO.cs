b23a165d54cb35b5d5c48ab2155102bd312610a2
sahilmore388 <sahilsanjaymore388@gmail.com>
2026-07-27 09:11:26 +0530

    Soft Delete And Dropdown Done

---
+++ Backend_Fincore.Application/DTOs/ExpenseClaim/ExpenseClaimDropdownDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Fincore.Application.DTOs.ExpenseClaim
+{
+    public class ExpenseClaimDropdownDTO
+    {
+        public int ExpenseClaimId { get; set; }
+        public string ClaimNumber { get; set; } = null!;
+    }
+}
